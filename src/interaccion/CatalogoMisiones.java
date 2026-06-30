0a564856d85a02927d9eb8bf2245a6e3e4446fa6
juanmunozz <juanmunoset35@gmail.com>
2026-06-30 18:25:29 -0300

    Misiones y CatalogoMisiones, por terminar

--- src/interaccion/CatalogoMisiones.java
+++ src/interaccion/CatalogoMisiones.java
@@ -1,5 +1,53 @@
 package interaccion;
 
-public class CatalogoMisiones {
+import java.util.ArrayList;
 
-}
+import recursos.Recurso;
+
+public class CatalogoMisiones {
+	
+
+	    private ArrayList<Mision> misiones;
+
+	    public CatalogoMisiones(ArrayList<Mision> misiones) {
+	    		
+	    	this.misiones = misiones;
+	    }
+
+	    
+
+	    public void mostrarMisiones() {
+
+	        for (int i = 0; i < misiones.size(); i++) {
+
+	            System.out.println((i + 1) + ". " + misiones.get(i).getNombre());
+
+	        }
+
+	    }
+	    private void cargarMisiones() {
+	    		
+	    	misiones.add(new Mision(/*NOMBRE, REQUISITOS, RECOMPENSA*/));
+
+	    	misiones.add(new Mision(/*NOMBRE, REQUISITOS, RECOMPENSA*/));
+
+	    	misiones.add(new Mision(/*NOMBRE, REQUISITOS, RECOMPENSA*/));
+	    	
+	    	
+	    }
+	    
+	    
+	    public void verificarMision(int posicion) {
+	    	
+	    }
+
+
+	    public Mision getMision(int posicion) {
+	    		
+	    	return misiones.get(posicion - 1);
+	    	
+	    }
+
+	}
+	
+
--- src/interaccion/Mision.java
+++ src/interaccion/Mision.java
@@ -1,5 +1,48 @@
 package interaccion;
 
+import java.util.ArrayList;
+
+import recursos.Recurso;
+
 public class Mision {
 
+	private String nombre;
+	private ArrayList<Recurso> requisitos; 
+	private boolean completada = false;
+	private int recompensa;
+	
+	
+	public Mision(String nombre, int recompensa, ArrayList<Recurso> requisitos) {
+		
+		this.nombre = nombre;
+		this.requisitos = requisitos;
+		this.recompensa = recompensa;
+		
+		
+		
+	}
+	
+	
+	public int getRecompensa() {
+		return recompensa;
+	}
+	
+	public boolean isCompletado() {
+		return completada;
+	}
+
+	
+	 public ArrayList<Recurso> getRequerimientos() { 
+		 return requisitos; 
+		 }
+	 
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void completar() {
+		completada = true;
+	}
+	
 }
